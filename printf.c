949ca5f48c0c5158b475e41ffd10bb5feb2aca21
Fasakin Henry <fasakinhenry@gmail.com>
2023-11-12 22:13:20 +0100

    Fix: Fix betty errors

--- printf.c
+++ printf.c
@@ -10,10 +10,10 @@ int _printf(const char *format, ...);
 
 int _printf(const char *format, ...)
 {
-	va_list args;
-	va_start(args, format);
-
 	int count = 0;
+	va_list args;
+
+	va_start(args, format);
 
 	while (*format != '\0')
 	{
@@ -23,12 +23,14 @@ int _printf(const char *format, ...)
 			if (*format == 'c')
 			{
 				int c = va_arg(args, int);
+
 				_putchar(c);
 				count++;
 			}
 			else if (*format == 's')
 			{
 				char *str = va_arg(args, char *);
+
 				while (*str != '\0')
 				{
 					_putchar(*str);
