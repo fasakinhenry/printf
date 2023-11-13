a2afe92407438950ecc07f0059d17f5ba576fffc
Fasakin Henry <fasakinhenry@gmail.com>
2023-11-13 22:53:57 +0100

    Feature: Add null validation for format in printf msin function

--- printf.c
+++ printf.c
@@ -43,6 +43,10 @@ int _printf(const char *format, ...)
 
 	if (format == NULL)
 		return (-1);
+	if (!format || (format[0] == '%' && !format[1]))
+		return (-1);
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
 	va_start(args, format);
 
 	while (*format != '\0')
