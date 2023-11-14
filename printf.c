195e68c023056f8e574dfff0a03ecc0a9ace400b
Fasakin Henry <fasakinhenry@gmail.com>
2023-11-14 19:23:39 +0100

    Fix: remove the code refactor

--- printf.c
+++ printf.c
@@ -109,13 +109,12 @@ int _printf(const char *format, ...);
 
 int _printf(const char *format, ...)
 {
-	if (format == NULL)
-		return (-1);
 	int count = 0;
 	va_list args;
 
 	va_start(args, format);
-
+	if (format == NULL)
+		return (-1);
 	if (!format || format == NULL || (format[0] == '%' && !format[1]))
 		return (-1);
 	if (format[0] == '%' && format[1] == ' ' && !format[2])
